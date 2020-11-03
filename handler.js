04bfb6bc2cece5c3fe0c78536bf92df6d51529d4
Chris Nesbitt-Smith <chris@cns.me.uk>
2020-11-03 20:29:20 +0000

    app code

---
+++ handler.js
@@ -0,0 +1,26 @@
+'use strict';
+const AWS = require('aws-sdk')
+
+const iot = new AWS.Iot()
+
+module.exports.create = params =>
+  iot.createThing(params).promise()
+
+
+module.exports.upsert = params => {
+  var response
+  try {
+    response = iot.updateThing(params).promise()
+  } catch (error) {
+    console.log(`couldn't update ${params.thingName} trying to create it`)
+    response = iot.createThing(params).promise()
+  }
+  return response
+}
+
+module.exports.update = params =>
+  iot.updateThing(params).promise()
+
+
+module.exports.delete = params =>
+  iot.deleteThing(params).promise()
