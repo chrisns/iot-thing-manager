3b293ee389e31c34239ccce2c96aabbbdbd9c53c
Chris Nesbitt-Smith <chris@cns.me.uk>
2020-11-10 09:56:57 +0000

    Revert "tweek upsert code"
    This reverts commit f5893c3899511d7af68a41bf681c417eae191cc7.

--- handler.js
+++ handler.js
@@ -5,13 +5,16 @@ const iot = new AWS.Iot()
 module.exports.create = params =>
   iot.createThing(params).promise()
 
-module.exports.upsert = params =>
-  iot.updateThing(params).promise()
-    .catch(() => {
-      console.log(`couldn't update ${params.thingName} trying to create it`)
-      return iot.createThing(params).promise()
-    })
-
+module.exports.upsert = params => {
+  var response
+  try {
+    response = iot.updateThing(params).promise()
+  } catch (error) {
+    console.log(`couldn't update ${params.thingName} trying to create it`)
+    response = iot.createThing(params).promise()
+  }
+  return response
+}
 
 module.exports.update = params =>
   iot.updateThing(params).promise()
